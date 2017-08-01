93ea6bc3be1bfe780590b607fa7e34b21cc3b2dd
John Peterson <jrpeter@vt.edu>
2017-08-01 12:57:03 -0400

    Enabled warnings removed some type conversions

--- src/piksi_surveyor.cpp
+++ src/piksi_surveyor.cpp
@@ -1,8 +1,12 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/TimeReference.h>
+#pragma GCC diagnostic pop
+
 #include <iostream>
 #include <sstream>
 #include <algorithm>
@@ -15,27 +19,19 @@
 #include <iomanip>
 
 
-int counter_gps = 0, counter_gpsrtk = 0;
-long double lat_acm = 0, lon_acm = 0, alt_acm = 0, east_acm = 0, north_acm = 0, up_acm = 0;
-double lat_avg = 0, lon_avg = 0, alt_avg = 0, east_avg = 0, north_avg = 0, up_avg = 0;
-int time_gps_sec = 0, time_gpsrtk_sec = 0, time_gps_nsec = 0, time_gpsrtk_nsec = 0, gps_stat = 0;
+size_t counter_gps = 0, counter_gpsrtk = 0;
+long double lat_acm = 0.0, lon_acm = 0.0, alt_acm = 0.0, east_acm = 0.0, north_acm = 0.0, up_acm = 0.0;
 
 std::ofstream gps_log, gpsrtk_log;
 
 
 void gps_callback(const sensor_msgs::NavSatFix  msg)
 {
-	
-
 	//Open file at initialization
 	if (counter_gps == 0){
 		gps_log.open("gps_log.txt");
 		gps_log << "Count, Seconds since epoch, NanoSeconds since second, Longitude, Latitude, Altitude, Status;\n";
 	}
-	
-	//Time
-	time_gps_sec = msg.header.stamp.sec;
-	time_gps_nsec = msg.header.stamp.nsec;
 
 	//Accumulators
 	lat_acm = lat_acm + msg.latitude;
@@ -46,21 +42,15 @@ void gps_callback(const sensor_msgs::NavSatFix  msg)
 	counter_gps++;
 
 	//Running Average
-	lat_avg = lat_acm/counter_gps;
-	lon_avg = lon_acm/counter_gps;
-	alt_avg = alt_acm/counter_gps;
-
-	//GPS Status
-	gps_stat = msg.status.status;
+	double lat_avg = lat_acm/counter_gps;
+	double lon_avg = lon_acm/counter_gps;
+	double alt_avg = alt_acm/counter_gps;
 
 	//Write to file
-	gps_log << counter_gps << "," << time_gps_sec << "," << time_gps_nsec << "," << std::fixed << std::setprecision(10) << msg.latitude << "," << std::fixed << std::setprecision(10) << msg.longitude << "," << std::fixed << std::setprecision(10) << msg.altitude << "," << gps_stat << ";\n";
+	gps_log << counter_gps << "," << msg.header.stamp.sec << "," << msg.header.stamp.nsec << "," << std::fixed << std::setprecision(10) << msg.latitude << "," << std::fixed << std::setprecision(10) << msg.longitude << "," << std::fixed << std::setprecision(10) << msg.altitude << "," << msg.status.status << ";\n";
 
 	//Output to view sample count and averaged location
-	ROS_INFO("[%f] lat	[%f] lon	[%f] alt	[%d] samples", lat_avg, lon_avg, alt_avg, counter_gps);
-
-
-
+	ROS_INFO("[%f] lat	[%f] lon	[%f] alt	[%lu] samples", lat_avg, lon_avg, alt_avg, counter_gps);
 }
 
 void gpsrtk_callback(const nav_msgs::Odometry msg)
@@ -70,10 +60,6 @@ void gpsrtk_callback(const nav_msgs::Odometry msg)
 		gpsrtk_log.open("gpsrtk_log.txt");
 		gpsrtk_log << "Count, Seconds since epoch, NanoSeconds since second, East(m), North(m), Up(m);\n";
 	}
-	
-	//Time
-	time_gpsrtk_sec = msg.header.stamp.sec;
-	time_gpsrtk_nsec = msg.header.stamp.nsec;
 
 	//Accumulators
 	east_acm = east_acm + msg.pose.pose.position.x;
@@ -84,23 +70,21 @@ void gpsrtk_callback(const nav_msgs::Odometry msg)
 	counter_gpsrtk++;
 
 	//Running Average
-	east_avg = east_acm/counter_gpsrtk;
-	north_avg = north_acm/counter_gpsrtk;
-	up_avg = up_acm/counter_gpsrtk;
+	double east_avg = east_acm / static_cast<double>(counter_gpsrtk);
+	double north_avg = north_acm / static_cast<double>(counter_gpsrtk);
+	double up_avg = up_acm / static_cast<double>(counter_gpsrtk);
 
 	//Write to file
-	gpsrtk_log << counter_gpsrtk << "," << time_gpsrtk_sec << "," << time_gpsrtk_nsec << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.x << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.y << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.z << ";\n";
+	gpsrtk_log << counter_gpsrtk << "," << msg.header.stamp.sec << "," << msg.header.stamp.nsec << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.x << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.y << "," << std::fixed << std::setprecision(10) << msg.pose.pose.position.z << ";\n";
 	
 	//Output to view sample count and averaged rtk location
-	ROS_INFO("[%f] East	[%f] North	[%f] Up	[%d] samples", east_avg, north_avg, up_avg, counter_gpsrtk);
-
+	ROS_INFO("[%f] East	[%f] North	[%f] Up	[%lu] samples", east_avg, north_avg, up_avg, counter_gpsrtk);
 }
 
 
 
 int main(int argc, char **argv)
 {
-
 	ros::init(argc, argv, "piksi_surveyor");
 
 	ros::NodeHandle n;
@@ -108,20 +92,7 @@ int main(int argc, char **argv)
 	ros::Subscriber sub_gps = n.subscribe("gps/fix", 1, gps_callback);
 	ros::Subscriber sub_gpsrtk = n.subscribe("gps/rtkfix", 1, gpsrtk_callback);
 
-	ros::Rate loop_rate(10);
-
-	int loopcount = 0;
-
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
-
-	while(ros::ok()){
-	
-		loopcount++;
-		ros::spinOnce();   	    
-		loop_rate.sleep();
-
-	}
-
+	ros::spin();
 
 	return 0;
 }
